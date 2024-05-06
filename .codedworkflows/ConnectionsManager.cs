ba5e83036eb30109af91285483918143cc51f5ce
tsxuehu <tsxuehu@qq.com>
2024-05-06 16:59:14 +0800

    init

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace BlankProcess1
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
---
+++ TestCase.cs
@@ -0,0 +1,40 @@
+using BlankProcess1.ObjectRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Excel;
+using UiPath.Excel.Activities;
+using UiPath.Excel.Activities.API;
+using UiPath.Excel.Activities.API.Models;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace BlankProcess1
+{
+    public class TestCase : CodedWorkflow
+    {
+        [TestCase]
+        public void Execute()
+        {
+            // Arrange
+            Log("Test run started for TestCase.");
+
+            // Act
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+
+            // Assert
+            // To start using activities, use IntelliSense (CTRL + Space) to discover the available services, e.g. testing.VerifyExpression(...).
+        }
+    }
+}
